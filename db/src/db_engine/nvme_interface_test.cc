9082a6b1b6e1080e12bd4a661f7f6fe905541b79
timatm <a131677975@gmail.com>
2025-06-29 21:19:19 +0800

    empty

--- db/src/db_engine/nvme_interface_test.cc
+++ db/src/db_engine/nvme_interface_test.cc
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../db_nvme/nvme_interface.hh"
+#include "../db_nvme/../db_nvme/nvme_interface.hh"
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 
 int main(){
     printf("nvme_interface_test\n");
+    nmc_config_t *config;
     // nmc_config_t config;
     // init_nmc_config(&config);
     int err = 0;
@@ -16,6 +17,19 @@ int main(){
         perror("open nvme device");
         return 1;
     }
+    err = ims_init(config,fd);
+    // config.data_len = 4096; // data length
+    // void *data = aligned_alloc(4096, config.data_len);
+    // if (!data) {
+    //     perror("alloc");
+    //     close(fd);
+    //     return 1;
+    // }
+    // memset(data, 0, config.data_len);
+    // config.data = (char *)data;
+    // config.metadata_len = 0;
+    // config.metadata =  NULL;
+    // err = ims_nvme_write(config, fd);
     err = ims_init(fd);
     // config.data_len = 4096; // data length
     // void *data = aligned_alloc(4096, config.data_len);
@@ -37,7 +51,7 @@ int main(){
         printf("error code: %d\n", err);
     }
 
-    // free(data);
+    // // free(data);
     close(fd);
     return 0;
 }
